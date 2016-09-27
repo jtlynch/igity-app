63fe93f0bf9b7145cf542c7dbc0c97360a55705d
John Lynch <jtlynch67@Johns-Mac-mini.home>
2016-09-27 16:12:32 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+* this is hello world written in Java
+*/
+
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# igity-app
